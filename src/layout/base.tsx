eaa3657d454ab4d4335a2331dcfa6b3370c9debe
Svenlaa <mail@svenlaa.com>
2024-02-21 23:26:41 +0100

    Add tailwind

--- src/layout/base.tsx
+++ src/layout/base.tsx
@@ -2,10 +2,12 @@ import Html from '@kitajs/html';
 
 type BaseLayoutProps = { children: JSX.Element; title?: string };
 const BaseLayout = ({ children, title = 'Svenlaa' }: BaseLayoutProps) => {
+    // noinspection HtmlUnknownTarget
     return (
         <html lang="en">
             <head>
                 <title>{title}</title>
+                <link rel="stylesheet" href="style.css" />
             </head>
             <body>{children}</body>
         </html>
