56722bdde3ae1ec690c6412ab40f35eb821b70ea
Svenlaa <mail@svenlaa.com>
2024-02-25 22:34:42 +0100

    Add mobile responsive tag

--- src/layout/base.tsx
+++ src/layout/base.tsx
@@ -36,6 +36,7 @@ const BaseLayout = (props: Props) => {
                         rel="stylesheet"
                         href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css"
                     />
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 </head>
                 <body class="relative flex min-h-screen w-full flex-col bg-white dark:bg-stone-900">
                     <Header />
