50caffcabe403dd570767aaf316f4c170a1e7660
Svenlaa <mail@svenlaa.com>
2024-03-09 18:08:29 +0100

    Change RSS pubDate to RFC-822

--- src/util.ts
+++ src/util.ts
@@ -9,19 +9,5 @@ export const run = async (command: string, options: string[] = []) => {
         console.log(stdout, stderr);
     }
 };
-export const formatDateToRFC822 = (date: Date): string => {
-    const options = {
-        weekday: 'short',
-        year: 'numeric',
-        month: 'short',
-        day: '2-digit',
-        hour: '2-digit',
-        minute: '2-digit',
-        second: '2-digit',
-        timeZone: 'UTC', // Adjust timeZone as needed
-    } as const;
-
-    const d = new Date(date.setUTCHours(18, 10));
-
-    return new Intl.DateTimeFormat('en-US', options).format(d);
-};
+export const formatDateToRFC822 = (date: Date): string =>
+    new Date(date.setUTCHours(18, 10)).toUTCString();
