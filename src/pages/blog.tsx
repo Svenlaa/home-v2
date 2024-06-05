04e3418114f845723b325c45dc0d7e65977520dd
Svenlaa <mail@svenlaa.com>
2024-06-05 22:19:13 +0200

    Add RSS badge on /blog

--- src/pages/blog.tsx
+++ src/pages/blog.tsx
@@ -1,6 +1,6 @@
+import Html from '@kitajs/html';
 import type { BlogDatum } from '../index.tsx';
 import BaseLayout from '../layout/base.tsx';
-import Html from '@kitajs/html';
 import { formatDateYMD } from '../util.ts';
 
 const Page = ({ blogData }: { blogData: BlogDatum[] }) => {
@@ -9,6 +9,17 @@ const Page = ({ blogData }: { blogData: BlogDatum[] }) => {
     return (
         <BaseLayout class="text-lg flex mt-8 mx-2 flex-col md:flex-row lg:gap-8 gap-4 justify-center">
             <div class="flex flex-col h-min gap-4">
+                <div class="flex flex-row gap-4">
+                    <a
+                        href="/blog.xml"
+                        target="_blank"
+                        rel="noreferrer"
+                        class=" text-orange-400 font-bold text-2xl bg-white py-1 px-2 rounded-sm shadow-sm hover:text-orange-500 hover:bg-gray-50 hover:shadow-md "
+                    >
+                        <span class="bi bi-rss-fill mr-2" />
+                        RSS
+                    </a>
+                </div>
                 {blogData.map((i) => {
                     return (
                         <a href={i.path} class="flex flex-row flex-1 items-end group">
