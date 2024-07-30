a7c4f56c83aaaed2519be3d1d3ee078c57639ee9
Svenlaa <mail@svenlaa.com>
2024-07-30 14:50:33 +0200

    less padding on sustainability message

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -3,7 +3,7 @@ import { Html } from '@kitajs/html';
 const Footer = () => {
     return (
         <footer class="z-50 flex flex-row items-end justify-end content-end">
-            <span class="text-green-600 p-4 first:rounded-t-lg bg-gray-50/50 dark:bg-white/10">
+            <span class="text-green-600 p-3 first:rounded-t-lg bg-gray-50/50 dark:bg-white/10">
                 <i class="text-md pr-2">🍃</i>
                 Powered by renewable energy
             </span>
